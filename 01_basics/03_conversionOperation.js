1ca63e3efe9b2fd8e620539dd85e110c97dbb8f9
Sintoo-Kumar <145006039+Sintoo-Kumar@users.noreply.github.com>
2024-02-21 09:05:35 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "30"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber );
+
+//"33" = 33
+//"33asd" = NaN
+// true => 1; false =>0
+
+let isLoggedIn = "Sintoo"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"Sintoo" =>  true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
