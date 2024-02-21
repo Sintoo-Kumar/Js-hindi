e35e8f9095467b3c4614dd1ca32e298db4617e1e
Sintoo-Kumar <145006039+Sintoo-Kumar@users.noreply.github.com>
2024-02-21 09:36:47 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "30"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber );
+// console.log(typeof valueInNumber );
 
 //"33" = 33
 //"33asd" = NaN
@@ -13,7 +13,7 @@ console.log(typeof valueInNumber );
 let isLoggedIn = "Sintoo"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -22,5 +22,31 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**********Operations *****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+/*
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+*/
+
+let str1 = "Hello"
+let str2 = " Sintoo"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
