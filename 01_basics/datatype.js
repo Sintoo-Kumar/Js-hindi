c9549511a1ca3e16809eba6b555924c0deb90b3e
Sintoo-Kumar <145006039+Sintoo-Kumar@users.noreply.github.com>
2024-02-21 13:52:41 +0000

    detatype summary

---
+++ 01_basics/datatype.js
@@ -0,0 +1,34 @@
+// # primitive
+
+// 7 type :- String , Number , Boolean , null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 123456789122345n
+
+
+// Reference (non primitive)
+
+// Array , object , Functions
+
+const heros = ["saktiman", "naagraj","doga"]
+let myObj = {
+    name:"Sintoo",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log (typeof bigNumber);
