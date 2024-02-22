9b9cfc86d2461871c17e47ec0abb2a91ece52d7e
Sintoo-Kumar <145006039+Sintoo-Kumar@users.noreply.github.com>
2024-02-22 14:58:47 +0000

    Math and Num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8743
+
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//  **********math***********
+
+console.log(Math);
+// console.log(Math.abs(-4)); // only for  Math.abs use in negative value  
+
+// console.log(Math.abs(4));
+
+// console.log(Math.round(4.6)); // they can choose the valu of >0.5 = top value and <0.5 = lower value 
+
+// console.log(Math.ceil(4.2)); //only can choose top value
+
+// console.log(Math.floor(4.2)); //only can choose floor value
+
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+console.log(Math.random()); // they can value in 0 and 1
+console.log((Math.random()*10)+1); //they can value  in one digit then poin value
+console.log(Math.floor(Math.random()*10)+1); // also can floor value
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);  // any value also 10+ value  
+
+
+
